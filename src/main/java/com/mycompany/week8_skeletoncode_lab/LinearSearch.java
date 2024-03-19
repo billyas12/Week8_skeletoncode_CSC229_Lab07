3a672e2b05407b597e9b1b68e63f3f50c5288963
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-03-19 17:12:00 -0400

    To do 1

--- src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/LinearSearch.java
@@ -15,6 +15,26 @@ public class LinearSearch {
         int n = arr.length;
         // Todo 01: - complete the implementation of linear search and test your code  
          //         - prvoide asymptotic analysis of the developed solution
+        for (int i=0;i<n;i++){
+            if (arr[i] == x){
+                return i;
+            }
+        }
+        return -1;
     }
-    
+
+    public static void main(String[] args){
+        int[] arr = {1, 7, 9 , 12, 2, 7, 9, 2, 5};
+        int num = 2;
+        int index = search(arr, num);
+
+        if (index != -1){
+            System.out.println("Element at index " + index);
+        }
+        else{
+            System.out.println("Element not found");
+        }
+    }
 }
+// Time Complexity = O(n) - linear complexity
+// Space Complexity = O(1) constant complexity
