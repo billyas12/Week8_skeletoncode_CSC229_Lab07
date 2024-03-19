14485601e6ec193e98bb19221ded363498ce9e89
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-03-19 16:59:14 -0400

    To do 3

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BubbleSort.java
@@ -15,9 +15,26 @@ public class BubbleSort {
         for (outer = size - 1; outer > 0; outer--) { // counting down
             for (inner = 0; inner < outer; inner++) { // bubbling up
                 //ToDo 3: complete this algorithm, test it, provide its time complexity
+                if (a[inner] > a[inner + 1]){
+                    temp = a[inner];
+                    a[inner] = a[inner + 1];
+                    a[inner + 1] = temp;
+                }
             }
         }
     }
-    
-    
+
+    public  static void main (String[] args){
+        int[] arr = {33, 22, 90, 11, 55, 66};
+        int n = arr.length;
+        bubbleSort(arr, n);
+
+        System.out.println("The Sorted Array is: ");
+
+        for (int i=0;i<n;i++){
+            System.out.println(arr[i] + "");
+        }
+    }
 }
+// Time Complexity = O(n^2)
+// Space Complexity = O(1)
