f37ed47d584978e983c69258e5da103280aae775
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-03-19 16:49:12 -0400

    To do 2

--- src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/BinarySearch.java
@@ -28,6 +28,21 @@ public class BinarySearch {
         return index;
     }
     //ToDo 2: Call the above method and test the algorithm  
-    // provide time and space analysis 
+    // provide time and space analysis
+    public static void main(String[] args) {
+        int[] sortedArray = {1, 3, 5, 7, 9, 11, 13};
+        int key = 7;
+        int result = runBinarySearchIteratively(sortedArray, key, 0, sortedArray.length - 1);
+        if (result != Integer.MAX_VALUE) {
+            System.out.println("Element at index: " + result);
+        } else {
+            System.out.println("Element not in the array.");
+        }
+
+        // Time Complexity: O(logn)
+        // Space Complexity: O(1)
+    }
+
+
 
 }
