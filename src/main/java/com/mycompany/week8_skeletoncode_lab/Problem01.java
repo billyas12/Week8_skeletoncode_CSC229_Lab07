7089751fc8a7d5bb3f9e0ed9b82441a5b7b846de
billyas12 <145586511+billyas12@users.noreply.github.com>
2024-03-19 17:18:56 -0400

    To do 4

--- src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
+++ src/main/java/com/mycompany/week8_skeletoncode_lab/Problem01.java
@@ -13,8 +13,27 @@ public class Problem01 {
     public static long getSumOfPrimes(int n){
     // Todo 04: Develop a method that returns the sum of the prime numbers between 1 and n
     //          Test your solution
-    //          Analyze its space and time  
-    
+    //          Analyze its space and time
+        long sum = 0;
+        for (int i=2;i<=n;i++){
+            if (isPrime(i)){
+                sum = sum + i;
+            }
+        }
+        returm sum;
     }
-    
+
+    private static boolean isPrime(int num){
+        if (num <= 1){
+            return false;
+        }
+        for (int i=2;i*i<=num;i++){
+            if (num % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
+// Time Complexity = O(n*sqrt(n))
+// Space Complexity = O(1)
